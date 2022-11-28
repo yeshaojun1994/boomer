8cd7720a24ff21f47bea57ce19b8d4aa192e6d70
yeshaojun1994 <118986802+yeshaojun1994@users.noreply.github.com>
2022-11-28 12:08:39 +0800

    ysj test

--- runner.go
+++ runner.go
@@ -136,6 +136,7 @@ func (r *runner) spawnWorkers(spawnCount int, quit chan bool, spawnCompleteFunc 
 					case <-quit:
 						return
 					default:
+						// ysj add 1128
 						if r.rateLimitEnabled {
 							blocked := r.rateLimiter.Acquire()
 							if !blocked {
